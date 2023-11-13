a307e06451e9a3d659ae64299edae5b8d9fadeea
Thompson Bear <thompsonbear21@gmail.com>
2023-11-13 16:47:03 -0500

    Font install

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,17 @@
+<header class="flex h-14 justify-between items-center border-b border-b-gray-800">
+	<h1 class="text-lg text-gray-400 font-semibold">Thompson Bear</h1>
+	<nav class="space-x-2">
+		<a
+			class="py-1 px-2 border border-gray-800 text-gray-400 bg-gradient-to-tr from-gray-800 to-gray-900 rounded-md"
+			href="/">Home</a
+		>
+		<a
+			class="py-1 px-2 border border-gray-800 text-gray-400 bg-gradient-to-tr from-gray-800 to-gray-900 rounded-md"
+			href="/about">About</a
+		>
+		<a
+			class="py-1 px-2 border border-gray-800 text-gray-400 bg-gradient-to-tr from-gray-800 to-gray-900 rounded-md"
+			href="/blog">Blog</a
+		>
+	</nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
 	import '../app.postcss';
+	import Header from '$lib/components/Header.svelte';
 </script>
 
-<slot />
+<div class="w-full flex flex-col items-center">
+	<div class="max-w-[1200px] flex flex-col w-full">
+		<Header />
+		<slot />
+	</div>
+</div>
