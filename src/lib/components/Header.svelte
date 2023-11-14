8b45dffb428ee3d7f4193792920dafeb640a9e0e
Thompson Bear <thompsonbear21@gmail.com>
2023-11-14 17:45:02 -0500

    Header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,16 +1,16 @@
-<header class="flex h-14 justify-between items-center border-b border-b-gray-800">
+<header class="flex h-16 justify-between items-center">
 	<h1 class="text-lg text-gray-400 font-semibold">Thompson Bear</h1>
 	<nav class="space-x-2">
 		<a
-			class="py-1 px-2 border border-gray-800 text-gray-400 bg-gradient-to-tr from-gray-800 to-gray-900 rounded-md"
+			class="py-2 px-3 text-gray-400 bg-gradient-to-tr from-gray-800 to-gray-900 rounded-md"
 			href="/">Home</a
 		>
 		<a
-			class="py-1 px-2 border border-gray-800 text-gray-400 bg-gradient-to-tr from-gray-800 to-gray-900 rounded-md"
+			class="py-2 px-3 text-gray-400 bg-gradient-to-tr from-gray-800 to-gray-900 rounded-md"
 			href="/about">About</a
 		>
 		<a
-			class="py-1 px-2 border border-gray-800 text-gray-400 bg-gradient-to-tr from-gray-800 to-gray-900 rounded-md"
+			class="py-2 px-3 text-gray-400 bg-gradient-to-tr from-gray-800 to-gray-900 rounded-md"
 			href="/blog">Blog</a
 		>
 	</nav>
