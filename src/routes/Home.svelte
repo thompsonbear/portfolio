3620c857b64553bcc2b784fc24e2e9857a0284cb
Thompson Bear <thompsonbear21@gmail.com>
2024-01-16 10:30:27 -0500

    Added resume download and button

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -15,16 +15,27 @@
 					<h1 class="whitespace-nowrap text-3xl font-bold xs:text-4xl sm:text-6xl">
 						<span class="font-normal">Hi,</span> I'm Thompson
 					</h1>
-					<p class="text w-fit rounded-md text-gray-400 backdrop-blur-xl sm:text-lg xl:text-2xl">
+					<p class="w-fit rounded-md text-gray-400 backdrop-blur-xl xs:text-lg xl:text-2xl">
 						Developer, Designer, and Engineer
 					</p>
 				</div>
 
-				<a
-					href="#work"
-					class="pointer-events-auto my-12 grid w-fit place-items-center rounded-md bg-blue-600 px-8 py-3 text-xl font-medium duration-300 hover:bg-blue-500"
-					>See Work</a
-				>
+				<div class="my-12 flex flex-wrap gap-4">
+					<a
+						href="#work"
+						class="pointer-events-auto flex w-36 items-center justify-center rounded-md bg-blue-600 py-3 text-xl font-medium duration-300 hover:bg-blue-500"
+						>See Work</a
+					>
+					<a
+						target="_blank"
+						href="/downloads/ThompsonBear_Resume.pdf"
+						download
+						class="pointer-events-auto flex w-36 items-center justify-center gap-2 rounded-md border-2 border-gray-300 py-3 text-xl font-medium text-gray-300 backdrop-blur-md duration-300 hover:text-gray-100 focus:text-gray-100"
+					>
+						<iconify-icon icon="mdi:download"></iconify-icon>
+						<span> Resume </span></a
+					>
+				</div>
 			</div>
 		</div>
 	</div>
